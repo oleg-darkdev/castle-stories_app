ef061f7a7eae051088d6ec1eedf83610864da922
DarkDev <darkdev_brand@riseup.net>
2024-07-03 08:47:37 +0300

    upd api

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -7,8 +7,22 @@ import About from './ui/landing/About.svelte';
 import TheStories from './ui/landing/TheStories.svelte';
 import Features from './ui/landing/Features.svelte';
 import Cta from './ui/landing/Cta.svelte';
-// import Features from './ui/landing/Features.svelte';
-// import Features from './ui/landing/Features.svelte';
+import ScenariosCharacteristics from './ui/landing/ScenariosCharacteristics.svelte';
+import ScenatiosInformation from './ui/landing/ScenatiosInformation.svelte';
+import RelatedProducts from './ui/landing/RelatedProducts.svelte';
 
 
-export { Nav, CastlesSlider, Hero, Stats, Footer, About, TheStories, Features, Cta };
+export {
+	Nav,
+	CastlesSlider,
+	Hero,
+	Stats,
+	Footer,
+	About,
+	TheStories,
+	Features,
+	Cta,
+	ScenariosCharacteristics,
+  ScenatiosInformation,
+  RelatedProducts
+};
