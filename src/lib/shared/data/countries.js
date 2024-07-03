8ac53829b6715cefa5eae64b9a77c5fcb3f37fa6
DarkDev <darkdev_brand@riseup.net>
2024-07-03 06:45:32 +0300

    refactoring structure

--- src/lib/shared/data/countries.js
+++ src/lib/shared/data/countries.js
@@ -1,35 +1,144 @@
 const countries = [
-	{
-		title: 'Lorem Ipsum is simply',
-		desc: ``,
+  {
+    region: "Eastern Europe",
+		desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
 		href: '/',
-	},
-	{
-		title: 'Lorem Ipsum is simply',
-		desc: ``,
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+
+    subregions: [
+      { name: "Russia", notableAreas: [] },
+      { name: "Ukraine", notableAreas: [] },
+      { name: "Belarus", notableAreas: [] }
+    ]
+  },
+  {
+    region: "Western Europe",
+	desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+	href: '/',
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+    subregions: [
+      { name: "Britain", notableAreas: [] },
+      { name: "France", notableAreas: [] },
+      { name: "Germany", notableAreas: [] }
+    ]
+  },
+  {
+    region: "Central Europe",
+	desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
+	href: '/',
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+    subregions: [
+      { name: "Czechia", notableAreas: [] },
+      { name: "Austria", notableAreas: [] },
+      { name: "Poland", notableAreas: [] }
+    ]
+  },
+  {
+    region: "Southern Europe",
+		desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
 		href: '/',
-	},
-	{
-		title: 'Lorem Ipsum is simply',
-		desc: ``,
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+
+    subregions: [
+      { name: "Italy", notableAreas: [] },
+      { name: "Spain", notableAreas: [] },
+      { name: "Portugal", notableAreas: [] }
+    ]
+  },
+  
+  {
+    region: "Northern Europe",
+		desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
 		href: '/',
-	},
-	{
-		title: 'Lorem Ipsum is simply',
-		desc: ``,
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+
+    subregions: [
+      { name: "Scandinavia", notableAreas: [] },
+      { name: "Baltic States", notableAreas: [] }
+    ]
+  },
+  {
+    region: "Middle East",
+		desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
 		href: '/',
-	},
-	{
-		title: 'Lorem Ipsum is simply',
-		desc: ``,
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+
+    subregions: [
+      { name: "Levant", notableAreas: [] },
+      { name: "Mesopotamia", notableAreas: [] }
+    ]
+  },
+  {
+    region: "South Asia",
 		href: '/',
-	},
-	{
-		title: 'Lorem Ipsum is simply',
-		desc: ``,
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+	desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
+    subregions: [
+      { name: "Indian Subcontinent", notableAreas: [] }
+	]
+  },
+  {
+    region: "East Asia",
+    subregions: [
+		{ name: "China", notableAreas: [] }    ]
+  },
+  {
+    region: "North America",
 		href: '/',
-	},
-	
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+	desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
+    subregions: [
+      { name: "Northeastern USA", notableAreas: [] },
+      { name: "Canada", notableAreas: [] }
+    ]
+  },
+  {
+    region: "Africa",
+		href: '/',
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+	desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
+    subregions: [
+      { name: "North Africa", notableAreas: [] }
+    ]
+  },
+  {
+    region: "Latin America",
+		href: '/',
+	castles: 0,
+	scenarios: 0,
+	id: 0,
+	desc: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`],
+
+    subregions: [
+      { name: "Caribbean", notableAreas: [] }
+    ]
+  }
 ];
 
 export default countries;
