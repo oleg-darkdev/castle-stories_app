9abafce7ff8bf32d74fa6d555b31f13d51047595
DarkDev <darkdev_brand@riseup.net>
2024-07-03 06:44:08 +0300

    upd content

--- src/lib/shared/data/promoCastles.js
+++ src/lib/shared/data/promoCastles.js
@@ -1,13 +1,32 @@
 const promoCastles = [
 	{
-		mainImg: 'images/about-2.png',
-		coverImg: 'images/about-1.png',
-		title: 'Lorem Ipsum',		
-		shortDesc: `Lorem Ipsum is simply dummy text of the printing.`,
+		mainImg: 'images/promoCastles/promo-br-1.png',
+		coverImg: 'images/promoCastles/promo-br-2.png',
+		title: 'Lida Castle',		
+		shortDesc: `Lida Castle - Lida, Belarus, built in 1323. It was part of the line of defense against the Crusaders`,
 		link: '/'		
 	},
-	
-
+	{
+		mainImg: 'images/promoCastles/promo-pl-1.png',
+		coverImg: 'images/promoCastles/promo-pl-2.png',
+		title: 'Będzinski Castle',		
+		shortDesc: `southern Poland, 14th century. Played an important role in the defense of the Kingdom of Poland and then the Polish-Lithuanian Commonwealth.`,
+		link: '/'		
+	},
+	{
+		mainImg: 'images/promoCastles/promo-ukr-1.png',
+		coverImg: 'images/promoCastles/promo-ukr-2.png',
+		title: 'Akkerman Fortress',		
+		shortDesc: `Belgorod-Dnestrovsky, Odessa region, Ukraine. Located on the site of the ancient city of Tyre.`,
+		link: '/'		
+	},
+	// {
+	// 	mainImg: 'images/promoCastles/promo-rus-1.png',
+	// 	coverImg: 'images/promoCastles/promo-rus-2.png',
+	// 	title: 'lorem Castle',		
+	// 	shortDesc: ``,
+	// 	link: '/'		
+	// },
 ]
 
 
