9e95f660ac212a7eb01337ad44cce908fdbb9b33
DarkDev <darkdev_brand@riseup.net>
2024-07-08 06:26:34 +0300

    add animation

--- src/lib/shared/data/stats.js
+++ src/lib/shared/data/stats.js
@@ -2,17 +2,20 @@ const stats = [
 	{
 		img: 'images/stats/castles.svg',
 		count: '1600+',
-		title: 'Castles and fortifications have been added'
+    title: 'Castles and fortifications have been added',
+    id: 'castles'
 	},
 	{
 		img: 'images/stats/countries.svg',
 		count: '6+',
-		title: 'Various countries from all continents added'
+		title: 'Various countries from all continents added',
+    id: 'countries'
 	},
 	{
 		img: 'images/stats/scenario.svg',
 		count: '4+',
-		title: 'Game scenarios available'
+		title: 'Game scenarios available',
+    id: 'scenarios'
 	},
 
 ]
--- src/lib/widgets/ui/landing/Stats.svelte
+++ src/lib/widgets/ui/landing/Stats.svelte
@@ -1,4 +1,15 @@
 <script>
+
+  import { CountUp } from 'countup.js';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+        const castlesCountUp = new CountUp('castles', 1600, { enableScrollSpy: true, duration: 5 }),
+        scenariosCountUp = new CountUp('scenarios', 7, { enableScrollSpy: true, duration: 6 }),
+        countriesCountUp = new CountUp('countries', 4, { enableScrollSpy: true, duration: 7 });
+	});
+
+
 	import {stats} from '$shared';
 </script>
 
@@ -11,14 +22,14 @@
         <div class="w-layout-grid animation-grid">
           <img src="{stat.img}" loading="lazy"  sizes="100vw" alt="{stat.title} image" class="image-animation is-1">
           <div class="hd-block hide-mobile">
-            <h2 class="animation-heading is-1">{stat.count}</h2>
+            <h2 class="animation-heading is-1" id={stat.id}>{stat.count}</h2>
           </div>
           <div  class="text-block hide-mobile">
             <div class="animation-text is-1">{stat.title}</div>
           </div>
           <div class="animation-mobile-content">
             <div class="hd-block">
-              <h2 class="animation-heading is-1">{stat.count}</h2>
+              <h2 class="animation-heading is-1" id={stat.id}></h2>
             </div>
             <div class="text-block">
               <div class="animation-text is-1">{stat.title}</div>
