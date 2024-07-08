4af47fb6150ebe4a630a7f536553dbbbb019284e
DarkDev <darkdev_brand@riseup.net>
2024-07-08 06:08:31 +0300

    add new sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	// import {  } from '$lib/shared';
-	import { Nav, CastlesSlider, Hero, Stats, Footer, About, TheStories, Features, Cta } from '$lib/widgets';
+	import { Nav, CastlesSlider, Hero, Stats, Footer, About, TheStories, AboutAuthor, Features, Cta, Gallery, Steps, Divider, Faq, Scenarios, EvolutionProjectSteps, FullWidthBanner } from '$lib/widgets';
 	import { InfoCard } from '$lib/entities';
 </script>
 
@@ -17,15 +17,35 @@
 
 	<About />
 
+	<!-- <EvolutionProjectSteps />/ -->
+	
 	<TheStories />
   
 	<Features />
 
 	<Stats />
 
+
+
+	<Gallery />
+
+	<Scenarios />
+	
+	<Faq />
+
+	
 	<Cta />
 
 	<Footer />
+
+  <!-- <Steps /> -->
+  
+  <!-- <FullWidthBanner />
+
+  <Divider />
+
+  	<AboutAuthor /> -->
+
 
 	<!-- <InfoCard /> -->
 </div>
