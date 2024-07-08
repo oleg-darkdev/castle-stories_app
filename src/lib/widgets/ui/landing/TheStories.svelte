5dbb6902e718c752eb62880ba5c1fa818e60aedb
DarkDev <darkdev_brand@riseup.net>
2024-07-08 06:08:10 +0300

    upd content

--- src/lib/widgets/ui/landing/TheStories.svelte
+++ src/lib/widgets/ui/landing/TheStories.svelte
@@ -5,9 +5,9 @@
             <div class="heading-txt-block ctr-212">
                 <div class="heading-div">
                     <h3 class="text-is-green">
-                        Part of <br /> 
-                        <a href='https://the-stories.vercel.app/' target='_blank' class="serif text-[#a44936]">THE Stories</a>
-                        <br /> project
+                        Author & creator <br /> 
+                        <a href='https://oleg-darkdev.vercel.app/' target='_blank' class="serif text-[#a44936]">Oleg Medvedev</a>
+                        <br /> 
                     </h3>
                 </div>
 
@@ -16,29 +16,41 @@
 
                 <div class="txt-block ctr-448">
                     <p class="body-22">
-                        <span class="serif">Lorem Ipsum</span>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            <br />
-                        <br />
-                        <span class="serif">Lorem Ipsum</span>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        <!-- <span class="serif">Lorem Ipsum</span> -->
+                        Product Web Engineer-IT Enthusiast, Gamification Coach, and JavaScript Programmer with across various technologies, <strong> 4 years of experience in board game development and 3 years in web application development </strong> including startups and open-source projects. See my open sorce projects are available on <strong>
+                        <a class='text-style-link text-[#a44936] serif' href='https://github.com/oleg-darkdev' target="_blank">github profile.</a>
+                      </strong>
                     </p>
+                      <p class="body-22">
+                        <!-- <span class="serif">Lorem Ipsum</span> -->
+Engaged in merging the digital and physical worlds to create engaging and educational experiences. Сollaborated with several <strong> NGOs and museums in Belarus and Poland </strong> on various historical projects. </p>
+                <p class='body-22'> See my other historical projects and <strong> educational board games that combine history and modern technologies in my <a class='text-style-link text-[#a44936] serif' href='https://oleg-darkdev.vercel.app/' target="_blank"> portfolio.</a>
+                  </strong>
+                      </p>
+                      <p class="body-22">
+                        <!-- <span class="serif">Lorem Ipsum</span> -->
+If you have ideas for collaboration - <strong>say me Hello from <a class='text-style-link text-[#a44936] serif' href='https://www.linkedin.com/in/oleg-darkdev' target="_blank">linkedin.</a></strong>
+                <br />
+                <br />
+                  In head with a love for technologies, in heart with a love for people ❤️
+                                      </p>
+
                     <a
                         href='https://the-stories.vercel.app/' target='_blank'
                         class="main-btn muted w-inline-block"
                     >
-                        <div>About THE STORIES</div>
+                        <div>Get in touch</div>
                         <img alt="" loading="lazy" src="images/arrow.svg" class="arrow-icon" />
                     </a>
                 </div>
             </div>
             <div class="img-flex-right">
                 <img
-                    src="images/the-stories.png"
+                    src="images/oleg-archaeo.JPG"
                     loading="lazy"
                     sizes="(max-width: 479px) 92vw, (max-width: 767px) 95vw, (max-width: 3181px) 43vw, 1368px"
                     alt="THE Stories promo image"
-                    class="img-philosophy is-paralax"
+                    class="img-philosophy"
                 />
             </div>
         </div>
