3ac114f4067f52fed0010245b7b00237450f50d8
DarkDev <darkdev_brand@riseup.net>
2024-07-08 06:26:55 +0300

    intial

---
+++ src/lib/widgets/ui/landing/FullWidthBanner.svelte
@@ -0,0 +1,27 @@
+<section class="section is-about">
+  <div class="w-layout-grid about-header-grid">
+    <div class="txt-about-us">
+      <p style="" class="body-22 text-is-white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </p>
+    </div>
+    <div class="heading-div about-us">
+      <h1   class="text-is-white">Lorem Ipsum is simply</h1>
+      <h1   class="text-is-white">
+        <span class="serif margin-21">Lorem Ipsum is simply</span>
+      </h1>
+      <h1  class="text-is-white">Lorem Ipsum is simply</h1>
+    </div>
+  </div>
+</section>
+
+
+<style>
+  .block-right-about {
+      height: 30rem;
+    }
+
+  .block-right-about.eco {
+    height: 24.6744rem;
+  }
+</style>
