e9c5e352ba0ff58ee5a140e14651718789f9cd68
DarkDev <darkdev_brand@riseup.net>
2024-07-08 06:26:43 +0300

    upd text

---
+++ src/lib/widgets/ui/landing/Gallery.svelte
@@ -0,0 +1,23 @@
+<section class="section is-philosophy">
+  <div class="philo-flex-txt">
+    <div class="heading-txt-block is-philo">
+      <h4>
+        Castles and fortifications - 
+        <span class="serif text-[#a44936]">
+          from around the world 
+        </span> 
+         in a single educational board game.
+      </h4>
+      <p class="body-22">
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.      </p>
+    </div>
+
+    <img src="images/promoCastles/br-1.jpg" loading="lazy"  sizes="(max-width: 767px) 187.40625px, 23vw" alt="" class="abs-img _5">
+    <img src="images/promoCastles/br-2.jpg" loading="lazy" alt="Indecom Philosophy" class="abs-img _4">
+    <img src="images/promoCastles/pl-2.jpg" loading="lazy"  sizes="(max-width: 767px) 100vw, 20vw" alt="" class="abs-img _3">
+    <img src="images/promoCastles/pl-1.jpg"  loading="lazy"  sizes="(max-width: 767px) 212px, (max-width: 991px) 26vw, 27vw" alt=""  class="abs-img _2">
+    <img src="images/promoCastles/ukr-1.jpg"  loading="lazy" sizes="(max-width: 767px) 200.71875px, 25vw" alt=""  class="abs-img _1">
+  </div>
+  
+</section>
+<div class="space-160" />
