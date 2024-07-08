502f7a24f952953bc78ef768edc870bacfe58017
DarkDev <darkdev_brand@riseup.net>
2024-07-08 06:09:22 +0300

    upd content

--- src/lib/widgets/ui/landing/About.svelte
+++ src/lib/widgets/ui/landing/About.svelte
@@ -35,16 +35,24 @@
                         />
                     </div>
                     <div class="right-block">
+                        <div class="heading-div mb-10">
+                            <h4 class="text-is-green">
+                                Part of <br /> 
+                                <a href='https://the-stories.vercel.app/' target='_blank' class="serif text-[#a44936]">THE Stories</a>
+                                <br /> project
+                            </h4>
+                        </div>
+                        
                         <div class="txt-block ctr-448">
                             <p class="body-22">
                                 <span class="serif text-[#a44936]">THE Castles Stories</span>
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.                                <br />
                                 <br />
-                                <span class="serif text-[#a44936]">Oleg Medvedev - Author & creator.</span>
+                                <span class="serif text-[#a44936]">THE Castles Stories</span>
 
                                 dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.                            </p>
-                            <a href="https://oleg-darkdev.vercel.app/" target="_blank" class="main-btn muted w-inline-block">
-                                <div>Contant with ME</div>
+                            <a href="https://the-stories.vercel.app/" target="_blank" class="main-btn muted w-inline-block">
+                                <div>About THE Stories</div>
                                 <img alt="" loading="lazy" src="images/arrow.svg" class="arrow-icon" />
                             </a>
                         </div>
